33b0efb48ea09b2c2af2445c190222c8a9e13b77
Bruce Zhao <shuai.zhao@partner.bmw.com>
2021-11-01 22:54:54 +0800

    mkdir

---
+++ src/views/gamefi/GameFi.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="gamefi-earning">
+    <vant-button icon="" type="danger" size="large"></vant-button>
+  </div>
+</template>
+
+<script lang="ts">
+  import button from 'vant';
+
+  export default {
+    name: 'GameFi',
+  };
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <van-swipe class="my-swipe" :autoplay="5000" lazy-render>
+    <van-swipe-item v-for="image in images" :key="image">
+      <img :src="image"/>
+    </van-swipe-item>
+  </van-swipe>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'Home',
+    setup() {
+      const images = [
+        '1.jpg',
+        '2.jpg',
+        '3.jpg',
+      ];
+      return { images };
+    },
+  });
+</script>
+
+<style scoped>
+
+</style>
