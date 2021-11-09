dff01b8ba618a5131aba9059d39cfffc4aeec09c
axmiao <18610834353@163.com>
2021-11-09 22:02:18 +0800

    add resultPopup

---
+++ src/components/ResultPopup.vue
@@ -0,0 +1,65 @@
+<template>
+  <van-popup v-model:show='state.showSuccess' round position='bottom' closeable
+             @close='handleClose'>
+    <van-cell :title='title' class='van-cell-no-border'>
+      <template #icon>
+        <svg-icon :icon-class='iconClass' style='width:30px; height:30px;'
+                  class='right-icon-account'></svg-icon>
+      </template>
+    </van-cell>
+    <van-cell-group>
+      <div class='account-label-container'>
+        <p class='success-label'>
+          {{ intro }}
+        </p>
+      </div>
+      <van-cell v-if='buttonVisible'>
+        <van-button type='danger' block @click='goTo(`/${targetUrl}`)'>Go to {{ targetName }}
+        </van-button>
+      </van-cell>
+      <div class='account-safe-area-bottom'></div>
+    </van-cell-group>
+
+  </van-popup>
+</template>
+
+<script>
+import { defineComponent, reactive, watchEffect } from 'vue';
+
+export default defineComponent({
+  name: 'successPopup',
+  props: {
+    show: Boolean,
+    title: String,
+    iconClass: String,
+    intro: String,
+    targetUrl: String,
+    targetName: String,
+    buttonVisible: Boolean,
+  },
+  emits: ['closeSuccessPopup'],
+  setup(props, context) {
+    const state = reactive({
+      showSuccess: false,
+    });
+
+    function handleClose() {
+      context.emit('closeSuccessPopup');
+    }
+
+    watchEffect(() => {
+      state.showSuccess = props.show || false;
+    });
+    return { state, handleClose };
+  },
+});
+</script>
+
+<style scoped>
+.success-label {
+  margin-left: 16px;
+  margin-right: 16px;
+  font-size: 10pt;
+  color: #575962;
+}
+</style>
