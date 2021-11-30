fa159a2d0cf26d4aa1c51f3a7ad059819945fa1b
fanggang.wang <fanggang.wang@foxmail.com>
2021-11-30 20:21:41 +0800

    remove login infor when account or chain change

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -72,6 +72,7 @@
         const eventManager = Web3Provider.getInstance().getEventManager();
 
         eventManager.on('accountsChanged', () => {
+          localStorage.removeItem('signInfo');
           localStorage.removeItem('parentAddr');
           router.push({
             path: '/splashScreen',
@@ -80,6 +81,7 @@
 
         eventManager.on('NotBscChain', () => {
           localStorage.removeItem('parentAddr');
+          localStorage.removeItem('signInfo');
           router.push({
             path: '/splashScreen',
           });
